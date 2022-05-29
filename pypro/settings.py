c5b972ddc563e8830253c5157b43db764412fb5a
Raphael Moral <80533929+raphamoral@users.noreply.github.com>
2022-05-29 02:46:23 -0300

    changing the color to deploy

--- pypro/settings.py
+++ pypro/settings.py
@@ -16,6 +16,8 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 import dj_database_url
 from decouple import config, Csv
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
