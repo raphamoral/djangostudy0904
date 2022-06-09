352e80dab38c5808f89b45ae2c0a44c29ae5577a
Raphael Moral <80533929+raphamoral@users.noreply.github.com>
2022-06-09 17:53:23 -0300

    filter, video de acordo com slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def video(request,slug):
-    return render(request,"aperitivos/video.html")
+    videos ={'motivacao':{"titulo": "Video Aperitivo: Motivação", "youtube_id":"_UfhOwkwXZ4"},
+    'poo-ramalho':{"titulo": "Programação Orientada a Objetos - Com Luciano Ramalho", "youtube_id": "_EblOW9nfkNA"},}
+
+    video =videos[slug]
+
+    return render(request,"aperitivos/video.html", context={"video": video})
