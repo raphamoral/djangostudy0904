9353f88736b71eb64b2e90f7342be8dfc37ecd7e
raphadeveloper <raphamoraleng@gmail.com>
2022-06-22 20:52:37 -0300

    facade, modulo

---
+++ pypro/modulos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Modulo(models.Model):
+    titulo = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.titulo
+
+# Create your models here.
