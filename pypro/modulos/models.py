a4af902d7be57b2fd8b3dada5dbe00360ac1cb25
raphadeveloper <raphamoraleng@gmail.com>
2022-06-27 20:36:01 -0300

    modulo ,navbar

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-
-class Modulo(models.Model):
+from django.urls import reverse
+from ordered_model.models import OrderedModel
+class Modulo(OrderedModel):
     titulo = models.CharField(max_length=64)
+    publico =models.TextField()
+    descricao = models.TextField()
+    slug =models.SlugField(unique=True)
+    class Meta(OrderedModel.Meta):
+        pass
 
     def __str__(self):
         return self.titulo
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug':self.slug})
 
 # Create your models here.
--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
+def detalhe(request, slug):
+    pass
 # Create your views here.
